fe21e405683149c5bc2ab774114e5c6d99b69c55
dbant <dbant@LAPTOP-R1FQPLFT>
2021-06-29 18:23:06 -0700

    minions done

---
+++ src/_05_minion/Minion.java
@@ -0,0 +1,44 @@
+package _05_minion;
+
+public class Minion {
+	private String name; 
+		private int eyes; 
+		private String color; 
+		private String master;
+	Minion (String name, int eyes, String color, String master){
+		this.name= name;
+		this.color= color;
+		this.eyes=eyes;
+		this.master=master;
+	}
+	String getName() {
+		return name;
+	}
+	void setName(String name) {
+		this.name=name;
+	}
+	int getEyes() {
+		return eyes;
+	}
+	
+	void setEyes(int eyes) {
+		this.eyes=eyes;
+	
+	}
+	String getColor() {
+		return color;
+	}
+	void setColor(String color) {
+		this.color=color;
+}
+	
+	String getMaster() {
+		return master;
+	}
+	void setMaster(String master) {
+		this.master=master;
+	}
+	
+}
+
+	
